1ee7cefc7a79ab3a427c1b7522d6b3013e40eb01
TeniG <tenigada@gmail.com>
2023-10-18 17:40:00 +0530

    Added Sqflite package. database created, table created, insert and fetch records implemented.

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -18,10 +18,11 @@ class PlaceLocation {
 
 class Place {
   Place({
+    String? id,
     required this.name,
     required this.image,
     required this.placeLocation,
-  }) : id = uuid.v4();
+  }) : id = id ?? uuid.v4();
   final String id;
   final String name;
   final File image;
--- lib/providers/place_provider.dart
+++ lib/providers/place_provider.dart
@@ -1,13 +1,64 @@
+import 'dart:io';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
+import 'package:path/path.dart' as path;
 
 import 'package:flutter_favourite_places/models/place.dart';
+
+const datebaseName = "places.db";
+const userPlacesTable = "user_places";
+
+Future<Database> _getDatabase() async {
+  final dbPath = await sql.getDatabasesPath();
+  final db = await sql.openDatabase(path.join(dbPath, datebaseName),
+      onCreate: (db, version) {
+    return db.execute(
+        "CREATE TABLE $userPlacesTable(id TEXT PRIMARY KEY, name TEXT, image_path TEXT, lat REAL, lng REAL, address TEXT)");
+  }, version: 1);
+
+  return db;
+}
 
 class PlaceNotifier extends StateNotifier<List<Place>> {
   // We initialize the list of places to an empty list
   PlaceNotifier() : super([]);
 
-  void addPlace(Place places) {
-    state = [...state, places];
+  Future<void> getAllPlaces() async {
+    final db = await _getDatabase();
+    final placesData = await db.query(userPlacesTable);
+
+    final places = placesData
+        .map(
+          (row) => Place(
+            id: row['id'] as String,
+            name: row['name'] as String,
+            image: File(row['image_path'] as String),
+            placeLocation: PlaceLocation(
+                latitude: row['lat'] as double,
+                longitude: row['lng'] as double,
+                address: row['address'] as String),
+          ),
+        )
+        .toList();
+    state = places;
+  }
+
+  void addPlace(Place places) async {
+    final db = await _getDatabase();
+    db.insert(userPlacesTable, {
+      "id": places.id,
+      "name": places.name,
+      "image_path": places.image.path,
+      "lat": places.placeLocation.latitude,
+      "lng": places.placeLocation.longitude,
+      "address": places.placeLocation.address
+    });
+
+    state = [
+      places,
+      ...state,
+    ];
   }
 }
 
